7a8737743aab0c883c374bf268ddf689dec4a2f0
Ryan Yost <ryost217@gmail.com>
2017-08-06 13:37:11 -0500

    styling cards for main menu/nav

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,23 +7,23 @@ export default Ember.Controller.extend({
 
   cards: [
           {
-            title: 'Estimate Take-Home Pay Before Your First Paycheck',
-            description: 'You landed your first job',
-            status: 'live',
+            title: 'Estimate Your Take-Home Pay',
+            description: 'You landed a job and know how much money you\'ll make. But how much will actually hit your bank account after taxes and retirement savings?',
+            comingSoon: false,
             link: 'calculators.estimate-take-home-pay',
             type: 'calculator'
           },
           {
             title: 'Guide',
             description: 'You landed your first job',
-            status: 'live',
+            comingSoon: true,
             link: 'calculators.estimate-take-home-pay',
             type: 'guide'
           },
           {
             title: 'Budget',
             description: 'You landed your first job',
-            status: 'live',
+            comingSoon: true,
             link: 'calculators.estimate-take-home-pay',
             type: 'tool'
           },
