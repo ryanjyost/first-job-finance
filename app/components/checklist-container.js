e504c742d71aa774beaa26c88486baa84df3a154
Ryan Yost <ryost217@gmail.com>
2017-09-18 17:53:15 -0500

    prep new landing

--- app/components/checklist-container.js
+++ app/components/checklist-container.js
@@ -14,44 +14,6 @@ export default Ember.Component.extend({
   }),
 
   _currentChecklistItemIDArray: computed.mapBy('currentListInfo.items', 'id'),
-  //_currentChecklistItemIDArrayLength: computed('_currentChecklistItemIDArray')
-
-
-
-
-  // percentComplete: computed('currentList', 'currentListInfo', '_currentChecklistItemIDArray', 'checklistItemsCompleted', function(){
-  //   //subract 1 because id always one more than index
-  //   const updatedItemsCompleted = this.get('checklistItemsCompleted');
-  //   const currentChecklistItemIDArray = this.get('_currentChecklistItemIDArray');
-
-  //   //console.log(currentChecklistItemIDArray)
-
-  //   // const currentListItemsCompleted = updatedItemsCompleted.filter((itemID) =>{
-  //   //   return currentChecklistItemIDArray.includes(itemID);
-  //   // });
-
-  //   const currentListItemsCompleted = [];
-
-  //   for(let item of updatedItemsCompleted ){
-  //     if(currentChecklistItemIDArray.includes(item)){
-  //       currentListItemsCompleted.pushObject(item)
-  //     }
-  //   }
-
-  //   console.log(currentListItemsCompleted);
-
-  //   const percentComplete = currentListItemsCompleted.length / currentChecklistItemIDArray.length;
-
-  //   //console.log(percentComplete)
-
-  //   const progressBarLeft = this.$('.progress-bar--left');
-  //   progressBarLeft.css("width", `${(percentComplete)*100}%`);
-
-  //   const progressBarRight = this.$('.progress-bar--right');
-  //   progressBarRight.css("width", `${(1 - percentComplete)*100}%`)
-
-  //   return percentComplete;
-  // }),
 
   init(){
     this._super(...arguments);
@@ -67,27 +29,6 @@ export default Ember.Component.extend({
     this.set('checklistItemsCompleted', checklistItemsCompleted);
   },
 
-
-  // didInsertElement(){
-  //   this._super(...arguments);
-
-  //   const updatedItemsCompleted = this.get('checklistItemsCompleted');
-  //   const currentChecklistItemIDArray = this.get('_currentChecklistItemIDArray');
-
-  //   const currentListItemsCompleted = updatedItemsCompleted.filter((itemID) =>{
-  //     return currentChecklistItemIDArray.includes(itemID);
-  //   });
-
-  //   const percentComplete = currentListItemsCompleted.length / currentChecklistItemIDArray.length;
-
-  //   const progressBarLeft = this.$('.progress-bar--left');
-  //   progressBarLeft.css("width", `${(percentComplete)*100}%`);
-
-  //   const progressBarRight = this.$('.progress-bar--right');
-  //   progressBarRight.css("width", `${(1 - percentComplete)*100}%`)
-
-  // },
-
   actions: {
     toggleSidebar(){
       this.toggleProperty('sidebarOpen');
@@ -98,22 +39,7 @@ export default Ember.Component.extend({
     },
 
     updateProgressBar(){
-      // const updatedItemsCompleted = this.get('checklistItemsCompleted');
-      // const currentChecklistItemIDArray = this.get('_currentChecklistItemIDArray');
 
-      // const currentListItemsCompleted = updatedItemsCompleted.filter((itemID) =>{
-      //   return currentChecklistItemIDArray.includes(itemID);
-      // });
-
-      // const percentComplete = currentListItemsCompleted.length / currentChecklistItemIDArray.length;
-
-      // this.set('percentComplete', percentComplete);
-
-      // const progressBarLeft = this.$('.progress-bar--left');
-      // progressBarLeft.css("width", `${(percentComplete)*100}%`);
-
-      // const progressBarRight = this.$('.progress-bar--right');
-      // progressBarRight.css("width", `${(1 - percentComplete)*100}%`)
     }
   }
 });
--- app/routes/portals.js
+++ app/routes/portals.js
@@ -19,7 +19,55 @@ export default Ember.Route.extend({
       return a.id.slice(7) > b.id.slice(7) ? 1 : -1;
     });
 
-    return portalsSortedByID;
+    const defaultWebsites = [
+      {
+        "id":"portal_0",
+        "name":"First Job Finance Blog",
+        "portalInfo": "Not so shameless plug for our blog." ,
+        "link":"https://medium.com/first-job-finance"
+      },
+      {
+        "id":"portal_1",
+        "name":"Free Annual Credit Reports",
+        "portalInfo": "You get one free report from Equifax, Experian and TransUnion every year." ,
+        "link":"https://www.annualcreditreport.com/index.action"
+      },
+      {
+        "id":"portal_2",
+        "name":"IRS.gov",
+        "portalInfo": "Everything about taxes you could possibly need." ,
+        "link":"https://www.irs.gov/"
+      },
+      {
+        "id":"portal_3",
+        "name":"NerdWallet",
+        "portalInfo": "Articles and information about personal finance." ,
+        "link":"https://www.nerdwallet.com/"
+      },
+      {
+        "id":"portal_4",
+        "name":"Bankrate",
+        "portalInfo": "Articles and information about personal finance." ,
+        "link":"http://www.bankrate.com/"
+      },
+      {
+        "id":"portal_5",
+        "name":"DMV.org",
+        "portalInfo": "All the car information." ,
+        "link":"http://www.dmv.org/"
+      },
+      {
+        "id":"portal_6",
+        "name":"The Balance",
+        "portalInfo": "Articles and information about personal finance." ,
+        "link":"https://www.thebalance.com/"
+      },
+    ];
+
+    return Ember.RSVP.hash({
+      userPortals: portalsSortedByID,
+      defaultWebsites
+    });
 
   }
 });
